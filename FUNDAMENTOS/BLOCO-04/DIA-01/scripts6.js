e95b9c518b07ba64f2a6e21b34e0910281685c34
Isaac Franzen <isaac_lol_@outlook.com>
2022-02-02 17:23:01 -0300

    colocando aspas simples

--- FUNDAMENTOS/BLOCO-04/DIA-01/scripts6.js
+++ FUNDAMENTOS/BLOCO-04/DIA-01/scripts6.js
@@ -1,8 +1,8 @@
-let weekDay = "Domingo";
+let weekDay = 'Domingo';
 
-if(weekDay === "segunda-feira"||weekDay === "terça-feira"||weekDay === "quarta-feira"||weekDay === "quinta-feira"||weekDay === "sexta-feira"){
-    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+if(weekDay === 'segunda-feira'||weekDay === 'terça-feira'||weekDay === 'quarta-feira'||weekDay === 'quinta-feira'||weekDay === 'sexta-feira'){
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D')
 }
 else{
-    console.log("FINALMENTE, descanso merecido UwU")
+    console.log('FINALMENTE, descanso merecido UwU')
 }
