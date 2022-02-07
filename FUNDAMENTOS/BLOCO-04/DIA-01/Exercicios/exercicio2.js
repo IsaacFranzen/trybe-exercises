566c8b4814a494382f00d2f2e5de8f236719c053
Isaac Franzen <isaac_lol_@outlook.com>
2022-02-07 17:37:14 -0300

    atualizando e usando funçoes

--- FUNDAMENTOS/BLOCO-04/DIA-01/Exercicios/exercicio2.js
+++ FUNDAMENTOS/BLOCO-04/DIA-01/Exercicios/exercicio2.js
@@ -1,4 +1,4 @@
-const numero1 = 5;
+/* const numero1 = 5;
 const numero2 = 10;
 
 if(numero1>numero2){
@@ -7,3 +7,28 @@ console.log('O primeiro número é o maior.')
 else{
     console.log('O segundo número é o maior.')
 }
+ */
+function maiorNumero(numero1,numero2){
+    if(numero1 > numero2){
+        return numero1 + ' é maior que '+ numero2;
+    } else if (numero2 > numero1){
+        return numero2 + ' é maior que o '+ numero1;
+    }
+    else{
+        return 'Os números são iguais';
+    }
+}
+console.log(maiorNumero(10,100));
+
+/* function maiorNum(primeiroNum, segundoNum) {
+    if (primeiroNum > segundoNum) {
+      return primeiroNum + ' é maior que ' + segundoNum;
+    } else if (segundoNum > primeiroNum) {
+      return segundoNum + ' é maior que ' + primeiroNum;
+    } else {
+      return 'Os números são iguais';
+    }
+  }
+  
+  console.log(maiorNum(10, 20)); // 20 é maior que 10
+  */
